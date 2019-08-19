8337e81b166904d0f57bbfc0e32f404e88ab5bbf
JakubK <jakubkuniszewski1@gmail.com>
2019-08-19 13:30:41 +0200

    Create Database migration after seeding it and adding new Table for Media

---
+++ Migrations/20190819111230_SeedData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportLeagueAPI.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Settlements",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Players",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MediaUrl",
+                table: "Newses",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Medias",
+                columns: table => new
+                {
+                    Url = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medias", x => x.Url);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "Date", "Description", "Name" },
+                values: new object[] { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Cool Event" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medias");
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Settlements");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "MediaUrl",
+                table: "Newses");
+        }
+    }
+}
