41c29037a3ae997d69512db1199497daa5bc2a83
JakubK <jakubkuniszewski1@gmail.com>
2019-08-19 13:29:37 +0200

    Create Schema and add basic resolver for events

---
+++ GraphQL/LeagueQuery.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using GraphQL.Types;
+using SportLeagueAPI.Context;
+using SportLeagueAPI.GraphQL.Types;
+using SportLeagueAPI.Repositories;
+
+namespace SportLeagueAPI.GraphQL
+{
+    public class LeagueQuery : ObjectGraphType
+    {
+        public LeagueQuery(EventRepository eventRepository)
+        {
+            Field<ListGraphType<EventType>>("events", resolve: context => eventRepository.GetAll());
+        }
+    }
+}
