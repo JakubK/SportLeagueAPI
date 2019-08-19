94302aee0108975add4963d849981f3087169f88
JakubK <jakubkuniszewski1@gmail.com>
2019-08-19 14:21:49 +0200

    Add resolver for Media entities in EventType

--- GraphQL/Types/EventType.cs
+++ GraphQL/Types/EventType.cs
@@ -1,17 +1,22 @@
 using GraphQL.Types;
-using SportLeagueAPI.Context;
 using SportLeagueAPI.Models;
+using SportLeagueAPI.Repositories;
 
 namespace SportLeagueAPI.GraphQL.Types
 {
     public class EventType : ObjectGraphType<Event>
     {
-        public EventType()
+        public EventType(MediaRepository mediaRepository)
         {
             Field(x => x.Id);
             Field(x => x.Name);
             Field(x => x.Description);
             Field(x => x.Date);
+            Field<ListGraphType<MediaType>>("medias", 
+            resolve: context => 
+            {
+                return mediaRepository.GetMultipleByOwnerId(context.Source.Id);
+            });
         }
     }
 }
