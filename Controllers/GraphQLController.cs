debdb8da0337ff5516ce5a3ca6fd1b26d23e9bde
JakubK <jakubkuniszewski1@gmail.com>
2019-08-23 11:06:53 +0200

    Create GraphQLController for testing the Queries

---
+++ Controllers/GraphQLController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using EntityGraphQL;
+using EntityGraphQL.Schema;
+using Microsoft.AspNetCore.Mvc;
+using SportLeagueAPI.Context;
+
+namespace SportLeagueAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GraphQLController : ControllerBase
+    {
+        private readonly LeagueDbContext _dbContext;
+        private readonly MappedSchemaProvider<LeagueDbContext> _schemaProvider;
+
+        public GraphQLController(LeagueDbContext dbContext, MappedSchemaProvider<LeagueDbContext> schemaProvider)
+        {
+            this._dbContext = dbContext;
+            this._schemaProvider = schemaProvider;
+        }
+
+        [HttpGet]
+        public object Get(string query)
+        {
+            return RunDataQuery(new QueryRequest { Query = query });
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]QueryRequest query)
+        {
+            return Ok(RunDataQuery(query));            
+        }
+
+        private object RunDataQuery(QueryRequest query)
+        {
+            try
+            {
+                var data = _dbContext.QueryObject(query, _schemaProvider);
+                return data;
+            }
+            catch (Exception)
+            {
+                return HttpStatusCode.InternalServerError;
+            }
+        }
+    }
+}
