4c9e28dd4b295c944001461ceb4709e522b41f90
JakubK <jakubkuniszewski1@gmail.com>
2019-08-24 13:20:42 +0200

    Create a mutation called AddEvent that resolves the mutation and creates new Event based on passed args

---
+++ GraphQL/EventMutations.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using EntityGraphQL.Schema;
+using SportLeagueAPI.Context;
+using SportLeagueAPI.DTO;
+using SportLeagueAPI.Models;
+
+namespace SportLeagueAPI.GraphQL
+{
+    public class EventMutations
+    {
+        [GraphQLMutation]
+        public Expression<Func<LeagueDbContext,Event>> AddEvent(LeagueDbContext context, CreateEvent args)
+        {
+            var newEvent = new Event();
+
+            newEvent.Name = args.Name;
+            newEvent.Date = args.Date;
+            newEvent.Description = args.Description;
+
+            context.Events.Add(newEvent);
+            context.SaveChanges();
+
+            return ctx => context.Events.First(x => x.Id == newEvent.Id);
+        }
+    }
+}
