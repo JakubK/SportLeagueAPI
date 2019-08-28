6bd972d6ef9e99257567f88dbdbfcde3d40ad369
JakubK <jakubkuniszewski1@gmail.com>
2019-08-28 09:42:11 +0200

    Modify AddPlayer class and PlayerMutations for test purposes

--- GraphQL/PlayerMutations.cs
+++ GraphQL/PlayerMutations.cs
@@ -5,17 +5,28 @@ using EntityGraphQL.Schema;
 using SportLeagueAPI.Context;
 using SportLeagueAPI.DTO;
 using SportLeagueAPI.Models;
+using SportLeagueAPI.Services;
 
 namespace SportLeagueAPI.GraphQL
 {
     public class PlayerMutations
     {
+
         [GraphQLMutation]
         public Expression<Func<LeagueDbContext,Player>> AddPlayer(LeagueDbContext context, AddPlayer args)
         {
             var newPlayer = new Player();
             newPlayer.Name = args.Name;
-            newPlayer.Settlement = context.Settlements.First(x => x.Name == args.SettlementName);
+
+            Console.Error.WriteLine("We got this far");
+
+
+            // var newMedia = new Media();
+            // newMedia.Url = _mediaUploader.UploadMedia(args.File);
+
+            // context.Medias.Add(newMedia);
+
+            // newPlayer.MediaId = newMedia.Id;
 
             context.Players.Add(newPlayer);
             context.SaveChanges();
