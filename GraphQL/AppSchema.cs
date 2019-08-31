37c93214fb1e06dee5613645ec47a3618450ffd4
JakubK <jakubkuniszewski1@gmail.com>
2019-08-31 17:59:13 +0200

    Created queries for fetching top 5 by default records from each Table

--- GraphQL/AppSchema.cs
+++ GraphQL/AppSchema.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using EntityGraphQL.Schema;
 using SportLeagueAPI.Context;
 
@@ -13,6 +14,24 @@ namespace SportLeagueAPI.GraphQL
             schema.AddMutationFrom(new NewsMutations());
             schema.AddMutationFrom(new SettlementMutations());
             schema.AddMutationFrom(new PlayerMutations());
+
+            schema.AddField("topNews",new {
+                count = 5
+            }, (ctx, param) => ctx.Newses.OrderByDescending(x => x.Date).Take(param.count)
+            ,"Newses","List of Newses");
+
+            schema.AddField("topEvents",new {
+                count = 5
+            }, (ctx, param) => ctx.Events.OrderByDescending(x => x.Date).Take(param.count)
+            ,"Events","List of Events");
+
+            schema.AddField("topPlayers", new {
+                count = 5
+            }, (ctx,param) => ctx.Players.OrderByDescending(x => x.Scores.Sum(y => y.Value)).Take(param.count),"Players","List of top Players");
+
+            schema.AddField("topSettlements", new {
+                count = 5
+            }, (ctx,param) => ctx.Settlements.OrderByDescending(x => x.Players.Sum(y => y.Scores.Sum(z => z.Value))).Take(param.count),"Settlements","List of top Settlements");
 
             return schema;
         }
