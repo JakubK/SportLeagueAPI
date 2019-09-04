ad29ca82868bfbbc9f72cfee6c1935350f84e275
JakubK <jakubkuniszewski1@gmail.com>
2019-09-04 13:03:58 +0200

    Create a mutation that deletes an Event

--- GraphQL/EventMutations.cs
+++ GraphQL/EventMutations.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Linq.Expressions;
 using EntityGraphQL.Schema;
+using Microsoft.EntityFrameworkCore;
 using SportLeagueAPI.Context;
 using SportLeagueAPI.DTO;
 using SportLeagueAPI.Models;
@@ -20,11 +21,20 @@ namespace SportLeagueAPI.GraphQL
             newEvent.Description = args.Description;
 
             newEvent.Medias = context.Medias.Where(x => args.Links.Contains(x.Url)).ToArray();
-
+            
             context.Events.Add(newEvent);
             context.SaveChanges();
 
             return ctx => context.Events.First(x => x.Id == newEvent.Id);
+        }
+
+        [GraphQLMutation]
+        public Expression<Func<LeagueDbContext,Event>> DeleteEvent(LeagueDbContext context, DeleteEvent args)
+        {
+            var eventToRemove = context.Events.Include(x => x.Scores).Include(x => x.Medias).Where(x => x.Id == args.Id).First();
+            context.Remove(eventToRemove);
+            context.SaveChanges();
+            return ctx => eventToRemove;
         }
     }
 }
