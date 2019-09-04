c8dd277ea3b3c7d8996b1f68842f8d5048ed829a
JakubK <jakubkuniszewski1@gmail.com>
2019-09-04 13:45:12 +0200

    Create UpdateSettlement model for representing data to update settlements

---
+++ Models/UpdateSettlement.cs
@@ -0,0 +1,10 @@
+namespace SportLeagueAPI.Models
+{
+    public class UpdateSettlement
+    {
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public string Description {get;set;}
+        public string Link {get;set;}
+    }
+}
