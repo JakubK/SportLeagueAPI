80455bbf35cec5703d3240ba93444ca954844227
JakubK <jakubkuniszewski1@gmail.com>
2019-09-04 13:45:24 +0200

    Create mutations for deleting and updating the Settlements

--- GraphQL/SettlementMutations.cs
+++ GraphQL/SettlementMutations.cs
@@ -25,5 +25,37 @@ namespace SportLeagueAPI.GraphQL
 
             return ctx => context.Settlements.First(x => x.Id == newSettlement.Id);
         }
+
+        [GraphQLMutation]
+        public Expression<Func<LeagueDbContext,Settlement>> DeleteSettlement(LeagueDbContext context, DeleteItem args)
+        {
+            var settlementToRemove = context.Settlements.First(x => x.Id == args.Id);
+            context.Remove(settlementToRemove);
+            context.SaveChanges();
+            return ctx => settlementToRemove;
+        }
+
+        [GraphQLMutation]
+        public Expression<Func<LeagueDbContext,Settlement>> UpdateSettlement(LeagueDbContext context, UpdateSettlement args)
+        {
+            var settlementToUpdate = context.Settlements.First(x => x.Id == args.Id);
+
+            if(!string.IsNullOrWhiteSpace(args.Name))
+                settlementToUpdate.Name = args.Name;
+
+            if(!string.IsNullOrWhiteSpace(args.Description))
+                settlementToUpdate.Description = args.Description;
+
+            if(!string.IsNullOrWhiteSpace(args.Link))
+            {
+                context.Remove(settlementToUpdate.Media);
+                settlementToUpdate.Media.Url = args.Link;
+            }
+
+            context.Settlements.Update(settlementToUpdate);
+            context.SaveChanges();
+
+            return ctx => settlementToUpdate;
+        }
     }
 }
