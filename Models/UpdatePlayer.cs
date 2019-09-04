56ddf6622e9d3d865f1815c5fbf38ca5aa56c9c1
JakubK <jakubkuniszewski1@gmail.com>
2019-09-04 14:03:31 +0200

    Create UpdatePlayer model to represent data for updating the Player

---
+++ Models/UpdatePlayer.cs
@@ -0,0 +1,9 @@
+namespace SportLeagueAPI.Models
+{
+    public class UpdatePlayer
+    {
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public string Link {get;set;}
+    }
+}
