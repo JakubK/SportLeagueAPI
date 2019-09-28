2515f92348626062392836e9dac69bdb1925b269
JakubK <jakubkuniszewski1@gmail.com>
2019-09-28 19:30:19 +0200

    Rename Value to Points in points resolver of Player and Settlement Graph Types

--- GraphQL/AppSchema.cs
+++ GraphQL/AppSchema.cs
@@ -50,8 +50,8 @@ namespace SportLeagueAPI.GraphQL
             schema.AddField("playersByScore",ctx => ctx.Players.OrderByDescending(x => schema.Type<Player>().GetField("points")),"Players by score descending");
 
             //points fields
-            schema.Type<Settlement>().AddField("points", ctx => ctx.Players.Sum(x => x.Scores.Sum(y => y.Value)),"Total points of Settlement");
-            schema.Type<Player>().AddField("points",ctx => ctx.Scores.Sum(y => y.Value),"Player points");
+            schema.Type<Settlement>().AddField("points", ctx => ctx.Players.Sum(x => x.Scores.Sum(y => y.Points)),"Total points of Settlement");
+            schema.Type<Player>().AddField("points",ctx => ctx.Scores.Sum(y => y.Points),"Player points");
 
             //media fields replacement
             schema.Type<Settlement>().ReplaceField("media",ctx => ctx.Media.Url,"Url of Settlement Image");
